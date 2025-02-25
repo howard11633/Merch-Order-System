73c09954556df35fb63be668dfc9ecb7a2b3e53e
howard <howard11633@gmail.com>
2025-02-25 17:24:46 +0800

    build environment, rename artifact ID as backend

---
+++ backend/src/main/java/com/merchordersystem/backend/MerchOrderSystemApplication.java
@@ -0,0 +1,13 @@
+package com.merchordersystem.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MerchOrderSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MerchOrderSystemApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/merchordersystem/backend/MerchOrderSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.merchordersystem.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MerchOrderSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
