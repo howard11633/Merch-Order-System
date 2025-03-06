174c4cc35c22d55fdbb98e6f419dafbb525baaea
howard <howard11633@gmail.com>
2025-03-06 11:28:42 +0800

    build up mySQL Database; Add Folder Entity & Repository

---
+++ backend/src/main/java/com/merchordersystem/backend/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.merchordersystem.backend.repository;
+
+import com.merchordersystem.backend.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
